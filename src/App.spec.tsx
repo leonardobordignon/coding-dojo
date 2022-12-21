af4addf28c9fee7bfacfea5fd5603b3099bfb9d4
leonardobordignon <leonardob@juntoseguros.com>
2022-12-21 15:56:53 -0300

    dojo: finish dojo

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -10,4 +10,16 @@ describe("VIACEP", () => {
 
     expect(input).toHaveValue("000000000");
   });
+
+  it("cep valido deve preencher o formulario com os valores", async () => {
+    render(<App />);
+    const input = screen.getByPlaceholderText("digite o cep");
+
+    fireEvent.change(input, { target: { value: "82640610" } });
+    fireEvent.blur(input, {});
+
+    expect(
+      await screen.findByText("Rua Pedro Spisla Filho")
+    ).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,51 @@
 import React, { useState } from "react";
 import classes from "./App.module.scss";
-import { InputBase } from "junto-design-system";
+import axios from "axios";
 
 function App() {
   const [inputCep, setInputCep] = useState("");
+  const [inputLogradouro, setInputLogradouro] = useState("");
+  const [inputComplemento, setInputComplemento] = useState("");
+  const [inputBairro, setInputBairro] = useState("");
+  const [inputLocalidade, setInputLocalidade] = useState("");
+  const [inputUf, setInputUf] = useState("");
+  const [invalidCep, setInvalidCep] = useState(false);
+
+  const getCepOnBlur = () => {
+    if (inputCep.match(/^[0-9]{8}$/)) {
+      axios
+        .get(`https://viacep.com.br/ws/${inputCep}/json/`)
+        .then(function (response) {
+          setInvalidCep(false);
+          setInputLogradouro(response.data.logradouro);
+          setInputComplemento(response.data.complemento);
+          setInputBairro(response.data.bairro);
+          setInputLocalidade(response.data.localidade);
+          setInputUf(response.data.uf);
+        });
+    } else {
+      setInvalidCep(true);
+    }
+  };
 
   return (
     <div className={classes.changecolor}>
-      <InputBase
-        placeholder="digite seu Cep"
+      <input
+        placeholder="digite o cep"
         onChange={(e) => setInputCep(e.target.value)}
         value={inputCep}
+        onBlur={(e) => getCepOnBlur()}
       />
+      {!invalidCep && (
+        <form>
+          <input placeholder="logradouro" value={inputLogradouro} />
+          <input placeholder="complemento" value={inputComplemento} />
+          <input placeholder="bairro" value={inputBairro} />
+          <input placeholder="localidade" value={inputLocalidade} />
+          <input placeholder="UF" value={inputUf} />
+        </form>
+      )}
+      {invalidCep && <p>CEP INVALIDO</p>}
     </div>
   );
 }
